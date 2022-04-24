0eff48ccf0fe4f519dc1b1d40c960a3aa5993f0a
RobertForristall <robert.forristall@knights.ucf.edu>
2022-04-24 13:31:58 -0400

    Added users/login route

--- routes/users.js
+++ routes/users.js
@@ -48,4 +48,26 @@ router.route('/sendVerification/:email').get((req, res) => {
 
 })
 
+router.route('/login').post((req, res) => {
+
+    query_string = `select name, dob from Users where email = "${req.body.email}" and pass = "${req.body.pass}";`
+
+    db.query(query_string, (err, results, fields) => {
+        if (err) return res.status(400).json(err)
+        if (results[0] === undefined) return res.status(400).json('Invalid Login Credentials! Please try again.')
+
+        token = jwt.sign({data: req.body.email}, process.env.SECRET_KEY, {expiresIn: '1h'})
+
+        res.set('Content-Type', 'application/json')
+        res.json({
+            token: token,
+            email: req.body.email,
+            name: results[0].name,
+            dob: results[0].dob
+        })
+
+    })
+
+})
+
 module.exports = router
